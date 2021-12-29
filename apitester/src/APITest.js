68220967eee95a0995e23824eb6d12ae9c0049f0
su <wkdlfflxh@naver.com>
2021-12-29 17:36:27 +0900

    Feat : project init

--- apitester/src/APITest.js
+++ apitester/src/APITest.js
@@ -4,10 +4,31 @@ import { useFormik } from 'formik';
 import QueryString from 'qs';
 import { useLocation } from 'react-router';
 import axios from 'axios';
+
+import TextField from '@mui/material/TextField';
+
+import SendIcon from '@mui/icons-material/Send';
+import { Button } from '@mui/material';
+import InputLabel from '@mui/material/InputLabel';
+import { Typography } from '@mui/material';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { createTheme } from '@mui/material';
+
 import './APITest.css';
 
+const method = ['get', 'post', 'put', 'delete'];
+const theme = createTheme({
+  typography: {
+    fontSize: 12,
+  },
+});
+
 const APITest = () => {
+  //   const theme = useTheme();
   const [response, setResponse] = useState([]);
+  const [seletedMethod, setSelectedMethod] = React.useState([]);
   const location = useLocation();
   const queryData = QueryString.parse(location.search, {
     ignoreQueryPrefix: true,
@@ -26,6 +47,11 @@ const APITest = () => {
     },
   });
 
+  const handleChange = (event) => {
+    console.log(event);
+    setSelectedMethod(event.target.value);
+  };
+
   const handleSaveButton = async (event) => {
     let response;
     try {
@@ -41,33 +67,69 @@ const APITest = () => {
   return (
     <div>
       <div className="apitest-body">
-        <form id="my-form" onSubmit={formik.handleSubmit}>
-          <input
-            id="method"
-            name="method"
-            type="string"
-            onChange={formik.handleChange}
-            value={formik.values.method}
-            placeholder="input method"
-          />
-          <input
-            id="url"
+        <FormControl sx={{ m: 1, minWidth: 90 }} size="small">
+          <InputLabel id="demo-simple-select-autowidth-label">
+            <Typography theme={theme} color="">
+              Method
+            </Typography>
+          </InputLabel>
+          <Select
+            labelId="demo-simple-select-autowidth-label"
+            id="demo-simple-select-autowidth"
+            value={seletedMethod}
+            onChange={handleChange}
+            autoWidth
+            label="Method"
+          >
+            <MenuItem value="">
+              <Typography theme={theme} color="">
+                None
+              </Typography>
+            </MenuItem>
+            {method.map((e) => {
+              return (
+                <MenuItem value={e}>
+                  <Typography theme={theme} color="">
+                    {e}
+                  </Typography>
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+        <form
+          className="apitest-form"
+          id="my-form"
+          onSubmit={formik.handleSubmit}
+        >
+          <TextField
+            id="outlined-multiline-flexible url"
+            multiline
+            maxRows={3}
+            label="url"
+            variant="standard"
             name="url"
             type="string"
             onChange={formik.handleChange}
             value={formik.values.url}
             placeholder="input url"
           />
-          <input
-            id="header"
+          <TextField
+            id="standard-basic header"
+            label="header"
+            variant="standard"
             name="header"
             type="string"
             onChange={formik.handleChange}
             value={formik.values.header}
             placeholder="input header"
           />
-          <input
-            id="body"
+          <TextField
+            id="outlined-multiline-flexible body"
+            multiline
+            maxRows={20}
+            label="body"
+            variant="standard"
             name="body"
             type="string"
             onChange={formik.handleChange}
@@ -75,15 +137,18 @@ const APITest = () => {
             placeholder="input body"
           />
         </form>
+        <button
+          className="button"
+          variant="contained"
+          endIcon={<SendIcon />}
+          form="my-form"
+          type="submit"
+          onClick={handleSaveButton}
+        >
+          Send
+        </button>
       </div>
-      <button
-        form="my-form"
-        type="submit"
-        className="save-button"
-        onClick={handleSaveButton}
-      >
-        Save
-      </button>
+
       {response.map((e) => {
         return <div>{e.nickname}</div>;
       })}
